ebde076fccdfb3ae25c8b8de6ef28dfab56391a6
Luis Elizondo <lelizondo@gmail.com>
2013-10-04 20:06:21 -0500

    Cambios al template para que todo funcione

--- jquery_file_upload-form.tpl.php
+++ jquery_file_upload-form.tpl.php
@@ -1,43 +1,27 @@
-
-
-
 <!-- Redirect browsers with JavaScript disabled to the origin page -->
 <noscript><input type="hidden" name="redirect" value="http://blueimp.github.com/jQuery-File-Upload/"></noscript>
+
 <!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
-<div class="row fileupload-buttonbar">
-  <div class="span7">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+  <!-- The global progress information and the files table-->
+  <div class="progress-container row" style="display: none;">
+    <h4>Subiendo archivo. Por favor espere.</h4>
+    <div class="progress">
+      <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;">
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
     <!-- The fileinput-button span is used to style the file input field as button -->
     <span class="btn btn-success fileinput-button">
-      <i class="icon-plus icon-white"></i>
-      <span>Add files...</span>
+      <span class="glyphicon glyphicon-open"></span>
+      <span>Agregar archivo...</span>
       <input type="file" name="files[]" multiple>
     </span>
-    <button type="submit" class="btn btn-primary start">
-      <i class="icon-upload icon-white"></i>
-      <span>Start upload</span>
+    
+    <button class="btn btn-default cancel" id="descartar-archivos">
+      <span>Cancelar</span>
     </button>
-    <button type="reset" class="btn btn-warning cancel">
-      <i class="icon-ban-circle icon-white"></i>
-      <span>Cancel upload</span>
-    </button>
-    <button type="button" class="btn btn-danger delete">
-      <i class="icon-trash icon-white"></i>
-      <span>Delete</span>
-    </button>
-    <input class="toggle" type="checkbox">
-  </div>
-  <!-- The global progress information -->
-  <div class="span5 fileupload-progress fade">
-    <!-- The global progress bar -->
-    <div id="progress" class="progress progress-success progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-      <div id="progress-bar" class="bar" style="width:0%;"></div>
-    </div>
-    <!-- The extended global progress information -->
-    <div class="progress-extended">&nbsp;</div>
   </div>
 </div>
-<!-- The loading indicator is shown during file processing -->
-<div class="fileupload-loading"></div>
-<br>
-<!-- The table listing the files available for upload/download -->
-<table role="presentation" class="table table-striped"><tbody class="files" data-toggle="modal-gallery" data-target="#modal-gallery"></tbody></table>
